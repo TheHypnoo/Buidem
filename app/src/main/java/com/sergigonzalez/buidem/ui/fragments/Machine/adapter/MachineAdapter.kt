4a48942b8e598166351e6e4ac3cc6f9d957e4a0b
TheHypnoo <sergi.gonzalez.lloria@gmail.com>
2021-03-26 21:05:27 +0100

    Bottom Sheet Dialog in process.

--- app/src/main/java/com/sergigonzalez/buidem/ui/fragments/Machine/adapter/MachineAdapter.kt
+++ app/src/main/java/com/sergigonzalez/buidem/ui/fragments/Machine/adapter/MachineAdapter.kt
@@ -4,10 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.sergigonzalez.buidem.R
 import com.sergigonzalez.buidem.data.Machines
 import com.sergigonzalez.buidem.databinding.ItemMachineBinding
+
 
 class MachineAdapter(private val listMachines: List<Machines>) :
     RecyclerView.Adapter<MachineAdapter.MachineHolder>() {
@@ -27,8 +30,25 @@ class MachineAdapter(private val listMachines: List<Machines>) :
     class MachineHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val binding = ItemMachineBinding.bind(view)
 
+
         fun render(Machine: Machines) {
-            binding.cvMachine.animation = AnimationUtils.loadAnimation(view.context,R.anim.scale_up)
+            binding.root.setOnClickListener {
+                val bt = BottomSheetDialog(binding.root.context, R.style.BottomSheetTheme)
+                val view: View = LayoutInflater.from(binding.root.context)
+                    .inflate(R.layout.bottom_sheet_layout, null)
+                view.findViewById<View>(R.id.prueba).setOnClickListener {
+                    Toast.makeText(binding.root.context, "add to cart", Toast.LENGTH_LONG).show()
+                    bt.dismiss()
+                }
+                bt.setContentView(view)
+                bt.show()
+            }
+
+
+            binding.cvMachine.animation = AnimationUtils.loadAnimation(
+                view.context,
+                R.anim.scale_up
+            )
             binding.tvNameClientItem.text = Machine.nameClient
             binding.tvAddressItem.text = Machine.addressMachine
             binding.tvSerialNumberMachineItem.text = Machine.serialNumberMachine
